7091d6bf8a21715419761dabc40938dff5c8168c
Joffre Pilay <jpilay@capacitate-it.com>
2016-08-06 22:36:12 -0500

    Añadiendo los viewsets para API

--- backend/viewsets.py
+++ backend/viewsets.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
-from models import *
-from backend.serializers import *
+from .models import *
+from .serializers import *
 
 class BusPlateViewSet(viewsets.ModelViewSet):
     queryset = BusPlate.objects.all()
