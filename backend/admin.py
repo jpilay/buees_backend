b77fe2f6a323ccba1c5071b5eb733bcc827ffc5d
Joffre Pilay <jpilay@capacitate-it.com>
2016-08-07 00:13:41 -0500

    Añadiendo BusScheduleAdmin en admin (backend)

--- backend/admin.py
+++ backend/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from backend.models import *
 
+class BusScheduleAdmin(admin.ModelAdmin):
+  list_display = ['image']
+
+  def has_add_permission(self, request):
+    num_objects = self.model.objects.count()
+    if num_objects >= 1:
+      return False
+    else:
+      return True
+
 admin.site.register(BusPlate)
 admin.site.register(BusRoute)
 admin.site.register(BusPosition)
-admin.site.register(BusSchedule)
+admin.site.register(BusSchedule,BusScheduleAdmin)
