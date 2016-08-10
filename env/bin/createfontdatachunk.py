5e9862946664c4956c09e6bfc4b00d07ed2bcc90
Joffre Pilay <jpilay@capacitate-it.com>
2016-08-10 11:18:31 -0500

    Añadiendo virtual env en proyecto

---
+++ env/bin/createfontdatachunk.py
@@ -0,0 +1,4 @@
+#!/home/julio/Escritorio/Camellos/UESS/Tesis2/buees/env/bin/python
+# EASY-INSTALL-SCRIPT: 'Pillow==3.4.0.dev0','createfontdatachunk.py'
+__requires__ = 'Pillow==3.4.0.dev0'
+__import__('pkg_resources').run_script('Pillow==3.4.0.dev0', 'createfontdatachunk.py')
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/julio/Escritorio/Camellos/UESS/Tesis2/buees/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
