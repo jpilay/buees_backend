67ce2c685896a9ee11b129dcff79faaa401590df
Joffre Pilay <jpilay@capacitate-it.com>
2016-08-17 18:53:09 -0500

    Añadiendo url api en urls (buees)

--- buees/urls.py
+++ buees/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
@@ -32,6 +32,8 @@ router.register(r'BusSchedule', BusScheduleViewSet)
 urlpatterns = [
     url(r'^$', index, name='home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(router.urls)),
+    #url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^bus_location/', bus_location, name='bus_location'),
     url(r'^recovery_password/', recovery_password, name='recovery_password'),
     url(r'^signin/', signin, name='signin'),
