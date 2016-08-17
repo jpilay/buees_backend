f9248429d464f69e8a1b602ab4d39f324578599e
Joffre Pilay <jpilay@capacitate-it.com>
2016-08-16 22:11:34 -0500

    Comentando validaciones de contraseña para usuarios en settings (buees)

--- buees/settings.py
+++ buees/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
+'''
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -115,7 +115,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+'''
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
