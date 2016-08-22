6c408fdd60c6fbf91dc9494643edc4272063dbdf
Joffre Pilay <jpilay@capacitate-it.com>
2016-08-22 12:57:41 -0500

    Registrando modelo DriverPublication en administrador

--- backend/admin.py
+++ backend/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from backend.models import *
 
 class BusScheduleAdmin(admin.ModelAdmin):
-  list_display = ['image']
+  list_display = ['id', 'image']
 
   def has_add_permission(self, request):
     num_objects = self.model.objects.count()
@@ -11,7 +11,13 @@ class BusScheduleAdmin(admin.ModelAdmin):
     else:
       return True
 
+
+class DriverPublicationAdmin(admin.ModelAdmin):
+  list_display = ['description', 'hour', 'date', 'image', 'status']
+
+
 admin.site.register(BusPlate)
 admin.site.register(BusRoute)
 admin.site.register(BusLocation)
 admin.site.register(BusSchedule,BusScheduleAdmin)
+admin.site.register(DriverPublication, DriverPublicationAdmin)
