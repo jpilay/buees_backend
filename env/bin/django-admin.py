a68031ba4261f61d3c545790161b3ccfc9cef20a
Joffre Pilay <jpilay@capacitate-it.com>
2016-08-29 12:57:05 -0500

    Eliminando enviroment

--- env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/julio/Escritorio/Camellos/UESS/Tesis2/buees/env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- env/bin/pildriver.py
+++
@@ -1,4 +0,0 @@
-#!/home/julio/Escritorio/Camellos/UESS/Tesis2/buees/env/bin/python
-# EASY-INSTALL-SCRIPT: 'Pillow==3.4.0.dev0','pildriver.py'
-__requires__ = 'Pillow==3.4.0.dev0'
-__import__('pkg_resources').run_script('Pillow==3.4.0.dev0', 'pildriver.py')
