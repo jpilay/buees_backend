c4ff113ee3a8145c094d06918934ccca271a8497
Joffre Pilay <jpilay@capacitate-it.com>
2016-09-04 14:32:17 -0500

    Añadiendo Coordinator API en urls (buees)

--- buees/urls.py
+++ buees/urls.py
@@ -28,6 +28,7 @@ router.register(r'BusPlate', BusPlateViewSet)
 router.register(r'BusRoute', BusRouteViewSet)
 router.register(r'BusLocation', BusLocationViewSet)
 router.register(r'BusSchedule', BusScheduleViewSet)
+router.register(r'Coordinator', CoordinatorViewSet)
 router.register(r'DriverPublication', DriverPublicationViewSet)
 router.register(r'UserGroup', UserGroupViewSet)
 
