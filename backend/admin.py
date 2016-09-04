efbb9ac3d4d3a0c4351fc79db932c40087364c9e
Joffre Pilay <jpilay@capacitate-it.com>
2016-09-04 14:30:19 -0500

    Añadiendo UserAdmin in admin (backend)

--- backend/admin.py
+++ backend/admin.py
@@ -1,25 +1,35 @@
 from django.contrib import admin
 from backend.models import *
+from django.contrib.auth.admin import UserAdmin
 from push_notifications.models import APNSDevice
 
 class BusScheduleAdmin(admin.ModelAdmin):
-  list_display = ['id', 'image']
+    list_display = ['id', 'image']
 
-  def has_add_permission(self, request):
-    num_objects = self.model.objects.count()
-    if num_objects >= 1:
-      return False
-    else:
-      return True
+    def has_add_permission(self, request):
+        num_objects = self.model.objects.count()
+        if num_objects >= 1:
+          return False
+        else:
+          return True
 
 
 class DriverPublicationAdmin(admin.ModelAdmin):
-  list_display = ['bus_route', 'description', 'hour', 'date', 'image', 'status']
+    list_display = ['bus_route', 'description', 'hour', 'date', 'image', 'status']
 
+class UserProfileInline(admin.StackedInline):
+    model = UserProfile
+    can_delete = False
+    max_num=1
+
+class UserAdmin(UserAdmin):
+    inlines = (UserProfileInline,)
 
 admin.site.unregister(APNSDevice)
+admin.site.unregister(User)
 admin.site.register(BusPlate)
 admin.site.register(BusRoute)
 admin.site.register(BusLocation)
 admin.site.register(BusSchedule,BusScheduleAdmin)
 admin.site.register(DriverPublication, DriverPublicationAdmin)
+admin.site.register(User,UserAdmin)
