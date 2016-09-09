6fd6d9638a99d116e62c89b0bb0cc6bc79538658
Julio Realpe <jreapupito@gmail.com>
2016-09-09 15:41:39 -0500

    Añadiendo lista de despligue para BusLocation en admin (backend)

--- backend/admin.py
+++ backend/admin.py
@@ -14,22 +14,29 @@ class BusScheduleAdmin(admin.ModelAdmin):
           return True
 
 
+class BusLocationAdmin(admin.ModelAdmin):
+    list_display = ['bus_plate','bus_route', 'latitude', 'longitude', 'date']
+
+
 class DriverPublicationAdmin(admin.ModelAdmin):
     list_display = ['bus_route', 'description', 'hour', 'date', 'image', 'status']
+
 
 class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
     max_num=1
 
+
 class UserAdmin(UserAdmin):
     inlines = (UserProfileInline,)
+
 
 admin.site.unregister(APNSDevice)
 admin.site.unregister(User)
 admin.site.register(BusPlate)
 admin.site.register(BusRoute)
-admin.site.register(BusLocation)
+admin.site.register(BusLocation,BusLocationAdmin)
 admin.site.register(BusSchedule,BusScheduleAdmin)
 admin.site.register(DriverPublication, DriverPublicationAdmin)
 admin.site.register(User,UserAdmin)
